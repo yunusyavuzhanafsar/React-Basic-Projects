6b2d73908be22300b1a254e9e192f5290b9729ba
Yunus Yavuzhan Afşar <yunusyavuzhanafsar@gmail.com>
2025-10-15 14:04:53 +0300

    currency app added.

---
+++ currency-app/currency-app/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+import Currency from './Components/Currency'
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div style={{display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center'}}><Currency/></div>
+        
+    </>
+  )
+}
+
+export default App
---
+++ currency-app/currency-app/src/main.jsx
@@ -0,0 +1,8 @@
+import './index.css'
+
+import App from './App.jsx'
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('root')).render(
+    <App />
+)
